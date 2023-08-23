b71b17398d5b553faf5536290bb16b7e17cb6e37
William Holtam <22566733+WilliamHoltam@users.noreply.github.com>
2023-08-23 09:39:51 +0100

    uplift tf templatefile

--- terraform/stage/services/webserver-cluster/main.tf
+++ terraform/stage/services/webserver-cluster/main.tf
@@ -26,20 +26,17 @@ resource "aws_launch_configuration" "example" {
   image_id        = "ami-0c55b159cbfafe1f0"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
-  user_data       = data.template_file.user_data
+  user_data = templatefile(
+    "user-data.sh",
+    {
+      server_port = var.server_port
+      db_address  = data.terraform_remote_state.db.outputs.address
+      db_port     = data.terraform_remote_state.db.outputs.port
+    }
+  )
 
   lifecycle {
     create_before_destroy = true
-  }
-}
-
-data "template_file" "user_data" {
-  template = file("user-data.sh")
-
-  vars = {
-    server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
   }
 }
 
